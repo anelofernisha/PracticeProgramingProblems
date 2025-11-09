a0b8be4ca4b3b7e9898f30d62b3070a3ef5b3d56
anelofernisha <anelofernisha@gmail.com>
2025-11-09 12:06:06 +0530

    Create PrintingSquareUsingStar.swift

---
+++ PrintingSquareUsingStar.swift
@@ -0,0 +1,10 @@
+// input="enter a side of square",output= " make a square using this "*""
+// * * * *
+// * * * *
+// * * * *
+// * * * *
+// area of square = a*a
+var a = 4
+for i in 1...a{
+ print(String(repeating:"* ",count:a))
+}
