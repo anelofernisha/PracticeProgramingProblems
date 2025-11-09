b318826b658b4c44c8745f33904206657c8ec89e
anelofernisha <anelofernisha@gmail.com>
2025-11-09 13:48:50 +0530

    Update PrintingUsingStar.swift

--- PrintingUsingStar.swift
+++ PrintingUsingStar.swift
@@ -1,4 +1,4 @@
-// input="enter a side of square",output= " make a square using this "*""
+// 1, input="enter a side of square",output= " make a square using this "*""
 // * * * *
 // * * * *
 // * * * *
@@ -8,3 +8,29 @@ var a = 4
 for i in 1...a{
  print(String(repeating:"* ",count:a))
 }
+
+//2,
+//st// input="enter a side of rectancle",output= " make a rectangle using this "*""
+// * * * * * 
+// * * * * * 
+// * * * * * 
+
+// * * *
+// * * *
+// * * *
+// * * *
+var l = 12
+
+var b = 6
+
+	for i in 1...l{
+
+      print(String(repeating: "* ",count: b))
+        
+  	
+    }
+    
+    
+    
+    
+
